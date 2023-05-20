ec09a52d7a5a92cbdfae8912ec57abb5b618adc5
IslamWalid <islamwalid83@gmail.com>
2023-05-21 00:06:03 +0300

    feat(nvim): add highlight yanked text

--- nvim/lua/general-config/au.lua
+++ nvim/lua/general-config/au.lua
@@ -1,8 +1,22 @@
+local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("FileType", {
+  group = augroup("Help Resize", {}),
   pattern = "help",
   callback = function()
     vim.cmd("resize 13")
+  end,
+})
+
+-- Highlight yanked text
+autocmd("TextYankPost", {
+  group = augroup("Yank", {}),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 50,
+    })
   end,
 })
