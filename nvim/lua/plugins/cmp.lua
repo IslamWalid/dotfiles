0cef8b3d33acd774ddf8c6efd6e598c85013329a
IslamWalid <islamwalid83@gmail.com>
2023-05-29 20:01:44 +0300

    feat(nvim): add highlight group to nvim-cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -61,9 +61,9 @@ return {
         },
         sources = cmp.config.sources({
           { name = "ultisnips", keyword_length = 2 },
-          { name = "nvim_lsp", keyword_length = 2 },
-          { name = "buffer", keyword_length = 5 },
-          { name = "path" },
+          { name = "nvim_lsp",  keyword_length = 2 },
+          { name = "buffer",    keyword_length = 5 },
+          { name = "path",      keyword_length = 2 },
         }),
         formatting = {
           format = lspkind.cmp_format({
@@ -110,6 +110,26 @@ return {
           },
         },
       }
+    end,
+    config = function(_, opts)
+      require("cmp").setup(opts)
+
+      -- gray
+      vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" })
+      -- blue
+      vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#569CD6" })
+      vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" })
+      -- light blue
+      vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#9CDCFE" })
+      vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
+      vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" })
+      -- pink
+      vim.api.nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#C586C0" })
+      vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" })
+      -- front
+      vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#D4D4D4" })
+      vim.api.nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" })
+      vim.api.nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" })
     end,
   },
 }
