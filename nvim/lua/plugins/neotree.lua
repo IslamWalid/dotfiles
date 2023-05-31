55ea6356f41255fd43e2482823a4cfe4716adc48
IslamWalid <islamwalid83@gmail.com>
2023-06-01 01:34:58 +0300

    feat(nvim): `harpoon` integration with `neotree`

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -111,6 +111,30 @@ return {
               ["<C-u>"] = "move_cursor_up",
             },
           },
+          components = {
+            harpoon_index = function(config, node, _)
+              local Marked = require("harpoon.mark")
+              local path = node:get_id()
+              local succuss, index = pcall(Marked.get_index_of, path)
+              if succuss and index and index > 0 then
+                return {
+                  text = string.format(" ⥤ %d", index), -- <-- Add your favorite harpoon like arrow here
+                  highlight = config.highlight or "NeoTreeDirectoryIcon",
+                }
+              else
+                return {}
+              end
+            end,
+          },
+          renderers = {
+            file = {
+              { "icon" },
+              { "name",         use_git_status_colors = true },
+              { "harpoon_index" }, --> This is what actually adds the component in where you want it
+              { "diagnostics" },
+              { "git_status",   highlight = "NeoTreeDimText" },
+            },
+          },
         },
       })
 
