ebc9a528a460a342e101544b6b71642cc7e1df53
IslamWalid <islamwalid83@gmail.com>
2024-09-29 16:48:48 +0300

    feat: update dap configurations

--- nvim/lua/plugins/dap/dlv.lua
+++ nvim/lua/plugins/dap/dlv.lua
@@ -2,69 +2,60 @@ local M = {}
 
 M.setup = function()
   local dap = require("dap")
+  local dap_utils = require("dap.utils")
 
   dap.adapters.delve = {
     type = "server",
     port = "${port}",
     executable = {
-      command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
+      command = "dlv",
       args = { "dap", "-l", "127.0.0.1:${port}" },
     },
   }
 
   dap.configurations.go = {
     {
-      name = "Project",
-      type = "delve",
-      request = "launch",
-      mode = "debug",
-      program = "${fileDirname}",
-    },
-    {
-      name = "Project (with args)",
-      type = "delve",
-      request = "launch",
-      program = "${fileDirname}",
-      args = function()
-        local input = vim.fn.input("argv: ")
-        return vim.split(input or "", " ")
-      end,
-    },
-    {
       name = "Attach Process",
       type = "delve",
       request = "attach",
       mode = "local",
-      processId = require("dap.utils").pick_process,
-    },
-    {
-      name = "Executable",
-      type = "delve",
-      request = "launch",
-      mode = "exec",
-      program = function()
-        return vim.fn.getcwd() .. "/" .. vim.fn.input("executable path: ")
+      processId = function()
+        local process_name = vim.fn.input("Process name: ")
+        return dap_utils.pick_process({ filter = process_name })
       end,
     },
     {
-      name = "Tests",
+      name = "Test Package",
       type = "delve",
       request = "launch",
       mode = "test",
       program = "${fileDirname}",
     },
     {
-      name = "Specific Test",
+      name = "Attach Process (Givesync)",
+      type = "delve",
+      request = "attach",
+      mode = "local",
+      processId = function()
+        return dap_utils.pick_process({ filter = "./build/api" })
+      end,
+    },
+    {
+      name = "Test Package (Givesync)",
       type = "delve",
       request = "launch",
       mode = "test",
-      program = "${fileDirname}",
-      args = {
-        "--",
-        "-test.run",
-        function()
-          vim.fn.input("test name: ")
-        end,
+      program = function()
+        local main_pkg_dir = vim.fn.fnamemodify(vim.fn.expand("%:p:h") .. "/../", ":p")
+
+        for _, dir in ipairs(vim.fn.readdir(main_pkg_dir)) do
+          if string.match(dir, "spec$") ~= nil then
+            return main_pkg_dir .. dir
+          end
+        end
+      end,
+      env = {
+        GIVESYNC_CONFIG = "/home/islam/work/lasting-dynamics/givesync-api/play/config.test.local.yaml",
       },
     },
   }
